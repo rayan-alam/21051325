00e08db9e9f2b23fb1ed9a249a3b9cac1810a80f
Rayan Alam <rayanalam29@gmail.com>
2024-05-08 11:57:26 +0530

    product microservies update

--- productMicroservice/index.js
+++ productMicroservice/index.js
@@ -4,3 +4,34 @@ const app = express();
 
 const BASE_URL = 'https://test-server.com/api';
 
+const COMPANIES = {
+    AMZ: 'Amazon',
+    FLP: 'Flipkart',
+    SNP: 'Snapdeal',
+    MYN: 'Myntra',
+    AZO: 'AZO',
+};
+
+const CATEGORIES = [
+    'Phone',
+    'Computer',
+    'TV',
+    'Earphone',
+    'Tablet',
+    'Charger',
+    'Mouse',
+    'Keypad',
+    'Bluetooth',
+    'Pendrive',
+    'Remote',
+    'Speaker',
+    'Headset',
+    'Laptop',
+    'PC',
+];
+
+
+
+app.listen(9876, () => {
+    console.log("Server live on port 9876");
+});
